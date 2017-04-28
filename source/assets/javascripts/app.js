c2b59210cb8f98441c2d24fee0a4582fad7cdb0a
mouktar <malidaher@gmail.com>
2017-04-28 17:22:26 +0200

    progress visuel

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,24 +1,25 @@
-function progress(){
-  var x = document.getElementsByTagName("progress")[0].getAttribute("value");
-  // transform this in integer
-  proceed = true;
-  x = parseInt(x) ;
-while(proceed){
-if(x>=100)
-{
-    proceed = false;
-    break;
-}
-  //alert(x);
-  x += 1;
-document.getElementsByTagName("progress")[0].setAttribute("value", x);
-  var x = document.getElementsByTagName("progress")[0].getAttribute("value");
+function download() {
+  document.getElementsByTagName("progress").display = "inline";
+  progress();
 
-  x = parseInt(x) ;
-  alert(x);
-  // setTimeout rapel le meme fonction au bout d'un certain temps
 }
+function progress(){
+  var rate = document.getElementsByTagName("progress")[0].getAttribute("value");
+  var ratio = rate + "%";
+  document.getElementsByTagName("h2")[0].innerHTML = ratio;
+  rate = parseInt(rate)+10; 
+  document.getElementsByTagName("progress")[0].setAttribute("value", rate);
+  if(rate <= 100)
+  setTimeout(function() { progress()}, 1000);
+  else
+    end_loading();
 }
+
+function end_loading(){
+  document.getElementsByTagName("h2")[0].innerHTML = "download end ";
+
+}
+
 
 
 // maximum de ligne 10 lignes par fonction
