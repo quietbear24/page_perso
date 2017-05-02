1823317a152ad007b0ed5df4eb0e87412031e4f1
mouktar <malidaher@gmail.com>
2017-05-02 18:40:10 +0200

    best practice and guide style

--- source/assets/javascripts/app.js
+++ source/assets/javascripts/app.js
@@ -1,27 +1,29 @@
-function download() {
-  document.getElementsByTagName("progress").display = "inline";
-  progress();
-
-}
-function progress(){
-  var rate = document.getElementsByTagName("progress")[0].getAttribute("value");
-  var ratio = rate + "%";
-  document.getElementsByTagName("h2")[0].innerHTML = ratio;
-  rate = parseInt(rate)+10; 
-  document.getElementsByTagName("progress")[0].setAttribute("value", rate);
-  if(rate <= 100)
-  setTimeout(function() { progress()}, 1000);
-  else
-    end_loading();
+// Function that change progress element attribute
+function showProgress() {
+   var rate = document.getElementsByTagName("progress")[0].getAttribute("value");
+   var ratio = rate + "%";
+   document.getElementsByTagName("h2")[0].innerHTML = ratio;
+   rate = parseInt(rate) + 10; 
+   console.log( rate );
+   document.getElementsByTagName("progress")[0].setAttribute("value", rate);
+   if (rate <= 100) {
+       setTimeout( function() { showProgress() }, 1000);
+   } else {
+       endLoading();
+   }
 }
 
-function end_loading(){
-  document.getElementsByTagName("h2")[0].innerHTML = "download end ";
-
+// Function called at the end of loading
+function endLoading(){
+   document.getElementsByTagName("h2")[0].innerHTML = "download end ";
+   location.reload();
+   //document.getElementsByTagName("button").click();
+   //showProgress(); what the problem 
 }
 
-
-
+// function that stop the loading
+function endProgress() {
+}
 // maximum de ligne 10 lignes par fonction
 // indentation precise
 // Aucune variable de moins de trois lettres
